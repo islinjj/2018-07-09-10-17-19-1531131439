2c74dce86c3ba22bb52af5b351a018e9e66811dd
Vicky Lin <jiajun_work@126.com>
2020-07-23 00:00:20 +0800

    [OO][11][vicky] feature: Add four classes: Klass, Person, Student and Teacher.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,60 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> studentList;
+    private List<Teacher> teacherList;
+
+    public Klass(int number) {
+        this.number = number;
+        this.studentList = new ArrayList<>();
+        this.teacherList = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        for (Student stu : studentList) {
+            if (stu.id == student.id){
+                this.leader = student;
+                for(Teacher teacher: teacherList) {
+                    teacher.updateLeader(leader);
+                }
+                return;
+            }
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student student){
+        studentList.add(student);
+        for (Teacher teacher : teacherList) {
+            teacher.updateStudent(student);
+        }
+    }
+
+    public boolean isIn(Student student){
+        for (Student stu : studentList) {
+            return stu.getKlass().getNumber() == student.getKlass().getNumber();
+        }
+        return false;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teacherList.add(teacher);
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,81 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+
+    private Klass klass;
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+        this.classes = new LinkedList<>();
+    }
+
+    public Teacher(int id,String name, int age,LinkedList<Klass> classes) {
+        super(id,name, age);
+        this.classes = classes;
+        for (Klass klass : classes) {
+            klass.addTeacher(this);
+        }
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce(){
+        if (this.classes.size() == 0){
+            return super.introduce() + " I am a Teacher. " + "I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. " + "I teach Class " + formatClassesOutput(this.classes) + ".";
+    }
+
+    public String introduceWith(Student student){
+        if (isTeaching(student)){
+            return "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass : classes) {
+            return klass.isIn(student);
+        }
+        return false;
+    }
+
+    private String formatClassesOutput(LinkedList<Klass> classes) {
+        String classesNumber = "";
+        for(Klass klass: classes){
+            classesNumber += klass.getNumber() + ", ";
+        }
+        return classesNumber.substring(0, classesNumber.length() - 2);
+    }
+
+    public void updateStudent(Student student) {
+        System.out.printf(
+                "I am %s. I know %s has joined Class %d.\n",
+                getName(),
+                student.getName(),
+                student.getKlass().getNumber());
+    }
+
+    public void updateLeader(Student leader) {
+        System.out.printf(
+                "I am %s. I know %s become Leader of Class %d.\n",
+                getName(),
+                leader.getName(),
+                leader.getKlass().getNumber());
+    }
 }
