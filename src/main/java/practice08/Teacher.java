9fa43916e61c2ac1cbb6c8106c5368da0497f728
Vicky Lin <jiajun_work@126.com>
2020-07-22 21:31:51 +0800

    [OO][08][vicky] feature:[eva xu][feature] Add four class: person, student, teacher, klass.

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,33 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        if (this.klass == null){
+            return super.introduce() + " I am a Teacher. " + "I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. " + "I teach Class " + this.klass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass() == this.klass){
+            return "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
