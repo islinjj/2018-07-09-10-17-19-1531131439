cfb0a197bb7d5806a612da8c461566b77441e880
Vicky Lin <jiajun_work@126.com>
2020-07-22 22:05:57 +0800

    [OO][09][vicky] feature Add four classes: Person,Student,Teacher,Klass.

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,38 @@
 package practice09;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            Person person = (Person) obj;
+            return this.id == person.id;
+        }
+        return false;
+    }
 }
