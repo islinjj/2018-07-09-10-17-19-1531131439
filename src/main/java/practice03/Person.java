4feaffd9c698ecc52e57173f5a38896d9e93b5e2
vicky <jiajun_work@126.com>
2020-07-22 17:13:43 +0800

    [OO][04][vicky] feature:finish basic introduce.

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -22,6 +22,6 @@ public class Person {
     }
 
     public String introduce(){
-        return "My name is " + this.name + ". I am " + this.age + " years old.";
+        return String.format("My name is %s. I am %d years old.", this.name, this.age);
     }
 }
