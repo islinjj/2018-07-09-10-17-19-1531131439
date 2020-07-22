af1ba8c5b77f2b43ba40137acdb21cb0b56271ee
vicky <jiajun_work@126.com>
2020-07-22 18:47:13 +0800

    [OO][07][vicky] feature:finish a part of teacher class.

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,32 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = new Klass(-1);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if (this.klass.getNumber() == -1){
+            return super.introduce() + " I am a Teacher. " + "I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. " + "I teach Class " + this.klass + ".";
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass().equals(this.klass)){
+            return "My name is + " + super.getName() + ". I am " + super.getAge() + " old. I am a Teacher. I teach Jerry."
+        }
+    }
 }
