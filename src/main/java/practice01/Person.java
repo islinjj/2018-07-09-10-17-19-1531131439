b4d554b0c1797e5fff4fb447becbe8e8ffdd1959
vicky <jiajun_work@126.com>
2020-07-22 16:19:42 +0800

    practice 01 [vicky] feature:finish person class.

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,15 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void introduceSelf(){
+        System.out.println("My name is " + this.name + ". I am " + this.age + " years old.");
+    }
 }
